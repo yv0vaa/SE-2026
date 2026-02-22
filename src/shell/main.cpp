840c00cb9ca7608dc66b55661bae1597eaa5f9a7
Vladimir Zakharov <zakharov.2004@yandex.ru>
2026-02-22 21:41:10 +0300

    fix: fix test on ubuntu

--- src/shell/main.cpp
+++ src/shell/main.cpp
@@ -1,6 +1,11 @@
 #include "shell/shell.hpp"
 
+#include <csignal>
+
 int main() {
+    // На Linux запись в pipe с закрытым читающим концом даёт SIGPIPE (например echo x | false).
+    std::signal(SIGPIPE, SIG_IGN);
+
     shell::Shell shell;
     return shell.run();
 }
